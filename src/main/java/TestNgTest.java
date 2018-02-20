9090dc638fc4be4a35826bcd765669b1eb2d327d
sandeep <voonnasandeep@gmail.com>
2018-02-20 11:26:54 +0530

    'adding_java_files'

---
+++ src/main/java/TestNgTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNgTest {
+
+    public WebDriver driver;
+
+    @Test
+    public void sample() {
+        System.out.println("Sample method");
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("Before method");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("After method");
+    }
+
+}
